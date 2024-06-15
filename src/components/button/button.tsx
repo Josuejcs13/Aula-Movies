1a61bef28ff8291f30ae1100924c104200540dd3
cwesly <cauawesly@outlook.com>
2024-06-15 16:20:41 -0300

    feat: add button prop

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,20 +3,25 @@ type ButtonProps = {
   handleClick: () => void;
   className?: string;
   color?: string;
+  disabled?: boolean;
 };
 
 const Button = ({
   children,
   handleClick,
   className,
-  color = "bg-red-600",
+  color,
+  disabled = false,
 }: ButtonProps) => {
   return (
     <button
       className={`py-3 text-sm text-white rounded-sm font-bold shadow-sm
            hover:brightness-75 transition-all duration-300 ease-in-out
-          ${color} ${className}`}
+          ${color} ${className} ${
+        disabled ? "bg-gray-400 cursor-not-allowed" : color
+      } ${className}`}
       onClick={handleClick}
+      disabled={disabled}
     >
       {children}
     </button>
