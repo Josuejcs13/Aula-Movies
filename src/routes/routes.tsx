6476eac9a5ca5c05e340808328c9d1ee1ed9955b
josuejcs13 <josuejcs13@gmail.com>
2024-07-06 15:24:49 -0400

    feat: create route favorite

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../Pages/login";
 import Home from "../Pages/home/home";
 import Movie from "../Pages/movie/movie";
+import Favorites from "../Pages/favorites";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: "/movie/:id",
     element: <Movie />,
+  },
+  {
+    path: "/favorites",
+    element: <Favorites />,
   },
 ]);
 
