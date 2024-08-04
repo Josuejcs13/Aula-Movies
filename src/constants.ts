5c6124487388c479bbe02b4c7789640141b76105
josuejcs13 <josuejcs13@gmail.com>
2024-08-04 19:42:10 -0400

    refactor: change the file in the hook for the central file

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,7 @@
 export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const BASE_URL = "https://api.themoviedb.org/3";
+
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+export const IMAGE_BASE_MOVIE = "https://image.tmdb.org/t/p/original/";
--- src/hooks/constants.ts
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = "https://api.themoviedb.org/3";
-export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
-export const IMAGE_BASE_MOVIE = "https://image.tmdb.org/t/p/original/"
