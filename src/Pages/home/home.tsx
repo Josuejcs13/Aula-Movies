1b01e7d577317c0c90ff0820e16c5fd32b5dc32a
josuejcs13 <josuejcs13@gmail.com>
2024-08-09 22:57:49 -0400

    refactor: home style

--- src/Pages/home/home.tsx
+++ src/Pages/home/home.tsx
@@ -9,22 +9,27 @@ const Home = () => {
   const { topSearch, popularSeries } = useContext(SectionContext);
 
   return (
-    <div className="h-full bg-[#141414] flex text-white justify-center flex-col">
+    <div className="h-full bg-[#141414] flex text-white justify-center flex-col -mt-12">
       <HeaderNetflix />
-      <img
-        src="https://occ-0-748-1001.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWdIvNKs08e4fn9HHGPBn13KTTbsFRKsZDOqS9H15jb4pdHltfDIEi7MnTF_N9nasEXpEokeccEnZfYAYMAmg3krX-3r9DG6HWVt.webp?r=650"
-        alt="background home page"
-        className="w-full h-screen object-cover"
-      />
-      <div className="flex w-full overflow-hidden pl-14 flex-col">
+      <div className="relative">
+        <img
+          src="https://occ-0-748-1001.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWdIvNKs08e4fn9HHGPBn13KTTbsFRKsZDOqS9H15jb4pdHltfDIEi7MnTF_N9nasEXpEokeccEnZfYAYMAmg3krX-3r9DG6HWVt.webp?r=650"
+          alt="background home page"
+          className="w-full h-screen object-cover"
+        />
+        <div className="absolute z-50 h-full w-full bg-gradient-to-t from-[#141414] to-transparent top-0">
+          {" "}
+        </div>
+      </div>
+      <div className="flex w-full overflow-hidden pl-14 flex-col ">
         <div className="flex w-full flex-col">
-          <h1>Top Search</h1>
+          <h1 className="text-lg font-medium">Top Search</h1>
           <CarouselMovies movies={topSearch} className="w-screen" />
         </div>
         <div className="flex w-full flex-col">
           {Boolean(favorites.length) && (
             <>
-              <h1>My List</h1>
+              <h1 className="text-lg font-medium">My List</h1>
               <CarouselMovies
                 movies={favorites}
                 className="w-screen"
@@ -33,7 +38,7 @@ const Home = () => {
           )}
         </div>
         <div className="flex w-full flex-col">
-          <h1>Popular Series</h1>
+          <h1 className="text-lg font-medium">Popular Series</h1>
           <CarouselMovies movies={popularSeries} className="w-screen" />
         </div>
       </div>
